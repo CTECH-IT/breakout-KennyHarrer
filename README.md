b75e3aab814641caca401018c519b92d0a54bed7
Kenny Harrer <7751202@isd535.org>
2021-09-08 09:34:53 -0500

    Game object, Game classes, Game initialization
        - Added Game object
        - Started classes player, and ball
        - Added fill option to all shapes
        - renamed all shape classes to be lowercase
        - Initialize game function

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-Change Log:
+TODO:
+    [] - ensure we are drawing inside the canvas
+    [] - game object
+    [] - points
+    [] - game over
+    [] - sounds?
+    [] - refactor to prevent dumb developer errors 
+    [] - settings screen?
+    [] - multiple games with game select? (probbably not)
 
-V 0.1
- - Added canvas
- - Updated author information
- - Clean black theme, center canvas, canvas border
- - Added classes vec2, Square, Rectangle, and Circle
- - Added draw loop
- - Added this change log
+
+Change Log:
+    V 0.2
+    - Added Game object
+    - Started classes player, and ball
+    - Added fill option to all shapes
+    - renamed all shape classes to be lowercase
+    - initlize game function
+    V 0.1
+    - Added canvas
+    - Updated author information
+    - Clean black theme, center canvas, canvas border
+    - Added classes vec2, Square, Rectangle, and Circle
+    - Added draw loop
+    - Added this change log
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,52 @@ const ctx = canvas.getContext("2d")
 
 const fps = 60
 
+var Game = {
+    States:{
+        TITLE:   0,
+        PLAYING: 1,
+        OVER:    3,
+    },
+    State:undefined,
+    Player:undefined,
+    Ball:undefined,
+    Squares:undefined,
+}
+
+function InitializeGame(){
+    Game.State = Game.States.TITLE
+    Game.Player = new Player()
+    Game.Ball = new Ball()
+    Game.Squares = [] // TODO: FIND POSITIONS FOR SQUARES
+}
+
+class Player{
+    constructor(x,length,color){
+        this.xCoord = x
+        this.platformLength = length
+        this.color = color!=undefined?color:"#ffffff"// if color is undefined use white
+    }
+    get X(){
+        return this.xCoord
+    }
+    get length(){
+        return this.platformLength
+    }
+    setLength(length){
+        this.platformLength = length
+    }
+    setColor(color){
+        this.color = color!=undefined?color:"#ffffff"// if color is undefined use white
+    }
+
+    
+}
+class Ball{
+    constructor(){
+
+    }
+}
+
 class vec2{
     constructor(x,y){
         this.xCoord = x
@@ -15,31 +61,48 @@ class vec2{
         return this.yCoord
     }
 }
-class Square{
-    constructor(vec2,size,color){
+class square{
+    constructor(vec2,size,fill,color){
         ctx.beginPath();
         ctx.rect(vec2.x,vec2.y,size,size)
         ctx.fillStyle = color!=undefined?color:"#ffffff"// if color is undefined use white
-        ctx.fill()
+        if(fill) {
+            ctx.fill()
+            ctx.fillStyle = color!=undefined?color:"#ffffff"// if color is undefined use white
+        }else{
+            ctx.stroke()
+            ctx.strokeStyle = color!=undefined?color:"#ffffff"// if color is undefined use white
+        }
         ctx.closePath()
     }
 }
-class Rect{
-    constructor(vec2,width,height,color){
+class rect{
+    constructor(vec2,width,height,fill,color){
         ctx.beginPath();
         ctx.rect(vec2.x,vec2.y,width,height)
         ctx.fillStyle = color!=undefined?color:"#ffffff"// if color is undefined use white
-        ctx.fill()
+        if(fill) {
+            ctx.fill()
+            ctx.fillStyle = color!=undefined?color:"#ffffff"// if color is undefined use white
+        }else{
+            ctx.stroke()
+            ctx.strokeStyle = color!=undefined?color:"#ffffff"// if color is undefined use white
+        }
         ctx.closePath()
     }
 }
-class Circle{
-    constructor(vec2,size,color){
+class circle{
+    constructor(vec2,size,fill,color){
         ctx.beginPath();
         ctx.arc()
         ctx.arc(vec2.x,vec2.y,size,0,Math.PI*2)
-        ctx.fillStyle = color!=undefined?color:"#ffffff"// if color is undefined use white
-        ctx.fill()
+        if(fill) {
+            ctx.fill()
+            ctx.fillStyle = color!=undefined?color:"#ffffff"// if color is undefined use white
+        }else{
+            ctx.stroke()
+            ctx.strokeStyle = color!=undefined?color:"#ffffff"// if color is undefined use white
+        }
         ctx.closePath()
     }
 }
@@ -50,6 +113,6 @@ function drawLoop(){
     //!DO NOT REMOVE!
     ctx.clearRect(0,0,canvas.width,canvas.height) //clear canvas
 
-    new Square(new vec2(50,50), 30)
+    new square(new vec2(50,50), 30, false)
 
 }
